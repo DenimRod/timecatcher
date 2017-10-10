2026a42b8ebacd8002e5b305363eb91be4f9189d
Julian Strausz <julian.strausz@gmx.at>
2017-10-10 11:08:10 +0200

    Login implemented

--- src/pages/login/login.ts
+++ src/pages/login/login.ts
@@ -1,7 +1,9 @@
 import {Component} from '@angular/core';
 // import {bootstrap} from '@angular/platform-browser-dynamic';
 import 'rxjs/Rx'
-import { BackandService } from '@backand/angular2-sdk'
+import { BackandService } from '@backand/angular2-sdk';
+import { NavController } from 'ionic-angular';
+import { CrudPage } from '../crud/crud';
 
 @Component({
     templateUrl: 'login.html',
@@ -20,7 +22,12 @@ export class LoginPage {
   newPassword: string = '';
   confirmNewPassword: string = '';
 
-  constructor(private backand: BackandService) {
+// Heres tha real code
+  userInput:string = '';
+  public items:any[] = [];
+
+
+  constructor(private backand: BackandService, public navCtrl: NavController) {
     this.backand.user.getUserDetails().then(
       (res: any) => {
         if(res.data) {
@@ -37,77 +44,115 @@ export class LoginPage {
     );
   }
 
+    public checkUser(inputNumber:string){
 
-  public getAuthTokenSimple() {
-    this.auth_type = 'Token';
-    this.backand.signin(this.username, this.password)
-      .then((res: any) => {
-        this.auth_status = 'OK';
-        this.is_auth_error = false;
-        this.loggedInUser = res.data.username;
-        this.username = '';
-        this.password = '';
-      },
-      (error: any) => {
-        let errorMessage: string = error.data.error_description;
-        this.auth_status = `Error: ${errorMessage}`;
-        this.is_auth_error = true;
-        this.auth_status = 'ERROR';
+
+      this.userInput += inputNumber;
+
+
+      if (this.userInput.length == 2){
+
+                    let params = {
+              filter: [
+                this.backand.helpers.filter.create('password', this.backand.helpers.filter.operators.text.equals, this.userInput),
+              ],
+            }
+
+        this.backand.object.getList('Users', params)
+         .then((res: any) => {
+           this.items = res.data;
+
+           if (this.items.length > 0) {
+              this.navCtrl.push(CrudPage);
+              this.userInput = '';
+           }
+           else {
+             alert ("This user doesn't exist!");
+
+             this.userInput = '';
+           }
+
+         },
+         (err: any) => {
+           alert(err.data);
+         });
+
+
+
       }
-    );
-  }
 
-  public useAnonymousAuth() {
-    this.backand.useAnonymousAuth()
-      .then((res: any) => {
-        this.auth_status = 'OK';
-        this.is_auth_error = false;
-        this.loggedInUser = res.data.username;
-      },
-      (error: any) => {
-        let errorMessage: string = error.data.error_description;
-        this.auth_status = `Error: ${errorMessage}`;
-        this.is_auth_error = true;
-        this.auth_status = 'ERROR';
-      });
-  }
-
-  public socialSignin(provider: string) {
-    this.backand.socialSignin(provider)
-      .then((res: any) => {
-        this.auth_status = 'OK';
-        this.is_auth_error = false;
-        this.loggedInUser = res.data.username;
-      },
-      (error: any) => {
-        let errorMessage: string = error.data.error_description;
-        this.auth_status = `Error: ${errorMessage}`;
-        this.is_auth_error = true;
-        this.auth_status = 'ERROR';
-      }
-    );
-  }
-
-  public signOut() {
-    this.auth_status = null;
-    this.backand.signout();
-  }
-
-
-  public changePassword() {
-    if (this.newPassword != this.confirmNewPassword){
-      alert('Passwords should match');
-      return;
     }
-    this.backand.changePassword(this.oldPassword, this.newPassword)
-      .then((res: any) => {
-        alert('Password changed');
-        this.oldPassword = this.newPassword = this.confirmNewPassword = '';
-      },
-      (err: any) => {
-        alert(err.data)
-      }
-    );
-  }
 
-}
+    public getAuthTokenSimple() {
+      this.auth_type = 'Token';
+      this.backand.signin(this.username, this.password)
+        .then((res: any) => {
+          this.auth_status = 'OK';
+          this.is_auth_error = false;
+          this.loggedInUser = res.data.username;
+          this.username = '';
+          this.password = '';
+        },
+        (error: any) => {
+          let errorMessage: string = error.data.error_description;
+          this.auth_status = `Error: ${errorMessage}`;
+          this.is_auth_error = true;
+          this.auth_status = 'ERROR';
+        }
+      );
+    }
+
+    public useAnonymousAuth() {
+      this.backand.useAnonymousAuth()
+        .then((res: any) => {
+          this.auth_status = 'OK';
+          this.is_auth_error = false;
+          this.loggedInUser = res.data.username;
+        },
+        (error: any) => {
+          let errorMessage: string = error.data.error_description;
+          this.auth_status = `Error: ${errorMessage}`;
+          this.is_auth_error = true;
+          this.auth_status = 'ERROR';
+        });
+    }
+
+    public socialSignin(provider: string) {
+      this.backand.socialSignin(provider)
+        .then((res: any) => {
+          this.auth_status = 'OK';
+          this.is_auth_error = false;
+          this.loggedInUser = res.data.username;
+        },
+        (error: any) => {
+          let errorMessage: string = error.data.error_description;
+          this.auth_status = `Error: ${errorMessage}`;
+          this.is_auth_error = true;
+          this.auth_status = 'ERROR';
+        }
+      );
+    }
+
+    public signOut() {
+      this.auth_status = null;
+      this.backand.signout();
+    }
+
+
+    public changePassword() {
+      if (this.newPassword != this.confirmNewPassword){
+        alert('Passwords should match');
+        return;
+      }
+      this.backand.changePassword(this.oldPassword, this.newPassword)
+        .then((res: any) => {
+          alert('Password changed');
+          this.oldPassword = this.newPassword = this.confirmNewPassword = '';
+        },
+        (err: any) => {
+          alert(err.data)
+        }
+      );
+    }
+
+  }
