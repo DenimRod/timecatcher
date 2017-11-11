ce53415c2a1ac58f7b1cb4986652829c93795429
Julian Strausz <julian.strausz@gmx.at>
2017-11-11 16:54:32 +0100

    Überflüssiger Code entfernt

--- src/pages/timestamp/timestamp.ts
+++ src/pages/timestamp/timestamp.ts
@@ -1,9 +1,7 @@
 import { Component } from '@angular/core';
-import { BackandService } from '@backand/angular2-sdk';
 import { GlobalVars } from '../../providers/globalvar';
 import { NavController } from 'ionic-angular';
 import { LoginPage } from '../login/login';
-import { App } from 'ionic-angular';
 
 
 @Component({
@@ -12,106 +10,14 @@ import { App } from 'ionic-angular';
 })
 export class TimestampPage {
 
-  public currentDate:string="";
-  public localDate:Date= null;
-
-  name:any="";
-  description:string = 'Wonderful';
-  public items:any[] = [];
-  searchQuery: string;
-
-  constructor(private backand: BackandService, public globVars: GlobalVars, public navCtrl: NavController, public app: App) {
+  constructor(public globVars: GlobalVars, public navCtrl: NavController ) {
 
     this.globVars.timer=this.globVars.logouttime;
     this.globVars.countDown();
 
   }
 
-public changeUser(){
-  this.navCtrl.push(LoginPage);
-}
-/*
-public startBreak(){
-  this.globVars.globCurrUser.status="Pause";
-  this.backand.object.update('Users', this.globVars.globCurrUser.id, this.globVars.globCurrUser);
-}
-
-public endBreak(){
-  if (this.globVars.globCurrUser.status=="Pause"){
-    this.globVars.globCurrUser.status="Arbeit";
-    this.backand.object.update('Users', this.globVars.globCurrUser.id, this.globVars.globCurrUser);
+  public changeUser(){
+    this.navCtrl.push(LoginPage);
   }
-}
-
-public endWork(){
-  this.globVars.globCurrUser.status="Außer Dienst";
-  this.backand.object.update('Users', this.globVars.globCurrUser.id, this.globVars.globCurrUser);
-}
-public startWork(){
-  this.globVars.globCurrUser.status="Arbeit";
-  this.backand.object.update('Users', this.globVars.globCurrUser.id, this.globVars.globCurrUser);
-
-  this.currentDate = (new Date()).toISOString();
-  this.localDate = new Date(this.currentDate);
-
-  this.backand.object.create('Timestamps', "{'date':'" + this.currentDate + "', 'status':'" + this.globVars.globCurrUser.status + "'}")
-}*/
-  public postItem() {
-    let item = {
-      name: this.name,
-      description: this.description
-    };
-
-    if (item.name && item.description) {
-      this.backand.object.create('todo', item)
-      .then((res: any) => {
-        // add to beginning of array
-        this.items.unshift({ id: null, name: this.name, description: this.description });
-        this.name = '';
-        this.description = '';
-      },
-      (err: any) => {
-        alert(err.data);
-      });
-    }
-  }
-
-  public getItems() {
-   this.backand.object.getList('todo')
-    .then((res: any) => {
-      this.items = res.data;
-    },
-    (err: any) => {
-      alert(err.data);
-    });
-  }
-
-  public filterItems() {
-    // set q to the value of the searchbar
-    var q = this.searchQuery;
-
-    // if the value is an empty string don't filter the items
-    if (!q || q.trim() == '') {
-      return;
-    }
-    else{
-        q = q.trim();
-    }
-
-
-    let params = {
-      filter: [
-        this.backand.helpers.filter.create('name', this.backand.helpers.filter.operators.text.contains, q),
-      ],
-    }
-
-    this.backand.object.getList('todo', params)
-    .then((res: any) => {
-      this.items = res.data;
-    },
-    (err: any) => {
-      alert(err.data);
-    });
-  }
-
 }
--- src/pages/timestamppro/timestamppro.ts
+++ src/pages/timestamppro/timestamppro.ts
@@ -1,10 +1,7 @@
 import { Component,ViewChild } from '@angular/core';
-import { BackandService } from '@backand/angular2-sdk';
 import { GlobalVars } from '../../providers/globalvar';
 import { NavController } from 'ionic-angular';
 import { LoginPage } from '../login/login';
-import { App } from 'ionic-angular';
-
 
 @Component({
   templateUrl: 'timestamppro.html',
@@ -13,42 +10,10 @@ import { App } from 'ionic-angular';
 export class TimestampProPage {
 @ViewChild('focusInput') myInput;
 
-//  public currentDate:string="";
-//  public localDate:Date= null;
+  constructor(public globVars: GlobalVars, public navCtrl: NavController) {
 
-
-  name:any="";
-  description:string = 'Wonderful';
-  public items:any[] = [];
-  searchQuery: string;
-
-  constructor(private backand: BackandService, public globVars: GlobalVars, public navCtrl: NavController, public app: App) {
-/*
-    this.searchQuery = '';
-    let that = this;
-    this.backand.on("items_updated",
-      (res: any) => {
-        alert("!");
-        let a = res as any[];
-        let newItem = {};
-        a.forEach((kv)=> newItem[kv.Key] = kv.Value);
-        that.items.unshift(newItem);
-      }
-    );*/
     this.globVars.timer=this.globVars.logouttime;
     this.globVars.countDown();
-  /*this.globVars.timer = 5;
-   this.globVars.logouttime=60000;
-  alert(this.globVars.logouttime);
-  setTimeout(function(){globVars.countDown()}, 1000);
-  setTimeout(function(){globVars.countDown()}, 2000);
-  setTimeout(function(){globVars.countDown()}, 3000);
-  setTimeout(function(){globVars.countDown()}, 4000);
-  setTimeout(function(){globVars.countDown()}, 5000);
-
-  setTimeout(()=>{this.app.getRootNav().setRoot(LoginPage );this.globVars.timer=5;},this.globVars.logouttime);
-  //  var TimeoutID = window.setTimeout(alert, 2000, "!");
-*/
   }
 
 ionViewDidEnter() {
@@ -66,63 +31,5 @@ public handleComment(){
 public changeUser(){
   this.navCtrl.push(LoginPage);
 }
-
-  public postItem() {
-    let item = {
-      name: this.name,
-      description: this.description
-    };
-
-    if (item.name && item.description) {
-      this.backand.object.create('todo', item)
-      .then((res: any) => {
-        // add to beginning of array
-        this.items.unshift({ id: null, name: this.name, description: this.description });
-        this.name = '';
-        this.description = '';
-      },
-      (err: any) => {
-        alert(err.data);
-      });
-    }
-  }
-
-  public getItems() {
-   this.backand.object.getList('todo')
-    .then((res: any) => {
-      this.items = res.data;
-    },
-    (err: any) => {
-      alert(err.data);
-    });
-  }
-
-  public filterItems() {
-    // set q to the value of the searchbar
-    var q = this.searchQuery;
-
-    // if the value is an empty string don't filter the items
-    if (!q || q.trim() == '') {
-      return;
-    }
-    else{
-        q = q.trim();
-    }
-
-
-    let params = {
-      filter: [
-        this.backand.helpers.filter.create('name', this.backand.helpers.filter.operators.text.contains, q),
-      ],
-    }
-
-    this.backand.object.getList('todo', params)
-    .then((res: any) => {
-      this.items = res.data;
-    },
-    (err: any) => {
-      alert(err.data);
-    });
-  }
 
 }
