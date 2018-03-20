a75527734e35dddbe3e52db269b0058919e3804c
liobas777 <strausz@aon.at>
2018-03-20 10:50:11 +0100

    JS-Test-File: Mehr Object/Function-Test

--- JS_Tests/Class_Objects.js
+++ JS_Tests/Class_Objects.js
@@ -4,13 +4,6 @@ Richie: das keyword function kann unterschiedliche Dinge: 1. function, <br>
 https://www.w3schools.com/js/tryit.asp?filename=tryjs_function_return <br>
 einfach den ganzen Text im Editor ersetzen -> RUN <br>
 */
-<!DOCTYPE html>
-<html>
-<body>
-
-<h2>JavaScript Functions</h2>
-
-<p>This example calls a function which performs a calculation and returns the result:</p>
 <p>Richie: das keyword function kann unterschiedliche Dinge: 1. function, 2. Objekte definieren</p>
 <p id="demo"></p>
 
@@ -20,21 +13,25 @@ function myFunction(a, b) {
     b=b+2;
     var inp = b;
     alert("b"+b);
-    this.logo=666+b;
-   return a * b;
+    this.attrib =666+b;
+    this.method = function(c, d){return c-d}; //Methode
+
+    return a * b;   // damit gezeigt wird, dass Class-Def + Fu-Def in EINEM !!!
 }
 alert("Test1");
 
-var t = new myFunction (7,8);
-alert("TestA");
+var t = new myFunction(3,4);
+alert("method: " + t.method(8,7));
+r = myFunction(7,8);
+alert("TestA"+t+r);
 //y = t(2,5);
 y=9;
-alert("Test2"+t.logo+"y:"+y);
+alert("Test2"+t.attrib+"y:"+y);
 
 var x = myFunction(4, 3);
 
 document.getElementById("demo").innerHTML = x;
-alert("Test3"+t.logo+":"+"x:"+x);
+alert("Test3"+t.attrib+":"+"x:"+x+"t:"+t.return);
 
 </script>
 
